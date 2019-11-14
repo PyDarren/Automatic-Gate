d443817f3ce786e4a75dc522cff8b2a1c37a9446
GIT <chenda4701@163.com>
2019-11-14 14:39:40 +0800

    DNA分类模型

--- rate_calculation.py
+++ rate_calculation.py
@@ -26,10 +26,11 @@ def ratioCalculation2(df, model):
     pre_1_length = len([i for i in pre_labels if i == 1])
     length_df = df.shape[0]
     df['class'] = pre_labels
+    sub_df = df[df['class']==0]
     ratio_0 = pre_0_length / length_df * 100
     ratio_1 = pre_1_length / length_df * 100
     ratio_list = [ratio_0, ratio_1]
-    return ratio_list
+    return ratio_list, sub_df
 
 
 
@@ -41,6 +42,12 @@ if __name__ == "__main__":
     model_CD66b = tf.keras.models.load_model('C:/Users/pc/OneDrive/PLTTECH/Project/01_自动圈门建模/Models/CD66b_classfy.h5')
     model_CD66b.build(input_shape)
 
+    model_beads = tf.keras.models.load_model('C:/Users/pc/OneDrive/PLTTECH/Project/01_自动圈门建模/Models/beads_classfy.h5')
+    model_beads.build(input_shape)
+
+    model_DNA = tf.keras.models.load_model('C:/Users/pc/OneDrive/PLTTECH/Project/01_自动圈门建模/Models/DNA_classfy.h5')
+    model_DNA.build(input_shape)
+
 
     ############################################
     ####          New sample test
@@ -50,11 +57,27 @@ if __name__ == "__main__":
     result_df = pd.DataFrame()
 
     for info in file_list:
+        info_list = list()
+        info_list.append(info[:-18])
         new_df = pd.read_csv(data_path+info).iloc[:, :-1]
-        ratio_CD66b = ratioCalculation2(new_df, model_CD66b)[:-1]
-        ratio_CD66b.insert(0, info[:-18])
-        info_df = pd.DataFrame(ratio_CD66b).T
-        info_df.columns = ['id', 'CD66b']
+        # 计算CD66b_比率
+        ratio_CD66b_all, CD66b_df = ratioCalculation2(new_df, model_CD66b)
+        ratio_CD66bNeg = ratio_CD66b_all[0]
+        info_list.append(ratio_CD66bNeg)
+        # 计算remove beads比率
+        CD66b_df_new = CD66b_df.iloc[:, :-1]
+        ratio_beads_all, beads_df = ratioCalculation2(CD66b_df_new, model_beads)
+        ratio_remove_beads = ratio_beads_all[0]
+        info_list.append(ratio_remove_beads)
+        # 计算DNA比率
+        beads_df_new = beads_df.iloc[:, :-1]
+        ratio_DNA_all, DNA_df = ratioCalculation2(beads_df_new, model_DNA)
+        ratio_DNA = ratio_DNA_all[0]
+        info_list.append(ratio_DNA)
+
+        info_df = pd.DataFrame(info_list).T
+        info_df.columns = ['id', 'CD66b_auto', 'Remove beads_auto', 'DNA_auto']
+        print(info_df)
         result_df = result_df.append(info_df)
         print('Sample %s has finished!' % info[:-18])
 
--- test.py
+++ test.py
@@ -9,11 +9,11 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    df_more = pd.read_csv('E:/cd/Automatic_Gate_Data/Rawdata/1_CD66b-.csv')
-    df_less = pd.read_csv('E:/cd/Automatic_Gate_Data/Rawdata/2_Removed beads.csv')
+    df_more = pd.read_csv('E:/cd/Automatic_Gate_Data/Rawdata/3_DNA.csv')
+    df_less = pd.read_csv('E:/cd/Automatic_Gate_Data/Rawdata/4_Singlets.csv')
 
     df = df_more.append(df_less)
     df = df.append(df_less)
-
     df_no_less = df.drop_duplicates(subset=list(df.columns), keep=False)
-    df_no_less.to_csv('E:/cd/Automatic_Gate_Data/Rawdata/nobeads.csv', index=False)
+
+    df_no_less.to_csv('E:/cd/Automatic_Gate_Data/Rawdata/noSinglets.csv', index=False)
